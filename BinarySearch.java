83ee08944294cf8cefb60ddc09cd6f8f53a97d49
Rowby Villanueva <116034103+rowby7@users.noreply.github.com>
2024-02-13 18:03:51 -0500

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,25 @@
+
+public class BinarySearch implements SearchInterface {
+
+	
+	
+	
+	
+	
+	
+	@Override
+	public int search(int[] listOfNumbers, int target) {
+		int low = 0;
+		int mid = 0;
+		int high = listOfNumbers.length-1;
+		
+		while (high >= low) {
+			mid = (high + low)/2;
+			
+			
+			
+		}
+		return -1;
+	}
+
+}
